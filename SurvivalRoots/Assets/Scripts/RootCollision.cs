45e6cead29b9a32507f237021e60b33ce1809ace
Peter Wallis <p.a.e.wallis@gmail.com>
2023-02-04 13:10:49 -0600

    Stump and Branches
    * Added stump
    * Split roots into their own class
    * Added root collision detection
    * Added the ability to draw multiple roots
    * Updated project settings for WebGL

---
+++ SurvivalRoots/Assets/Scripts/RootCollision.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RootCollision
+{
+    public RootLine parent = null;
+    public Vector2 point;
+    public float radius;
+    public float maxRadius;
+}
--- SurvivalRoots/Assets/Scripts/RootDraw.cs
+++ SurvivalRoots/Assets/Scripts/RootDraw.cs
@@ -5,130 +5,117 @@ using UnityEngine;
 public class RootDraw : MonoBehaviour
 {
     public LineRenderer playerLine;
-    public LineRenderer rootLine;
+    public RootLine rootLinePrefab;
     private Camera mainCamera;
+    public GameObject cursor;
+    public Transform[] startNodes;
 
     [Range(0,10)]public float maxLength = 5;
+    [Range(0, 5)] public float startRadius = 1;
+    [Range(0, 5)] public float rootRadius = 1;
     private float currentLength = 0;
     [Range(0.000001f, 0.01f)] public float resamplingSize = 0.001f;
     [Range(0, 0.05f)] public float resamplingNoise = 0.01f;
+    private bool isDrawing = false;
+    private List<RootLine> roots = new List<RootLine>();
+    private RootCollision currentRootStart;
 
 
     private void Start()
     {
         mainCamera = Camera.main;
+        cursor.transform.localScale = Vector3.one * (startRadius * 2);
     }
 
 
     private void Update()
     {
+        Vector2 pos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        RootCollision collision = GetCollision(pos);
+        bool canDraw = collision != null;
 
-        if (Input.GetMouseButtonDown(0))
+        cursor.SetActive(canDraw);
+        if (canDraw)
         {
-            playerLine.positionCount = 0;
-            currentLength = 0;
-        }
+            cursor.transform.localScale = Vector3.one * (collision.maxRadius * 2);
+            cursor.transform.position = collision.point;
 
-        if (Input.GetMouseButton(0))
-        {
-            Vector2 pos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
-            float delta = playerLine.positionCount > 1 ? ((Vector2)playerLine.GetPosition(playerLine.positionCount - 1) - pos).magnitude : 0;
-
-            if (currentLength + delta <= maxLength)
+            if (Input.GetMouseButtonDown(0))
             {
-                currentLength += delta;
-                playerLine.positionCount++;
-                playerLine.SetPosition(playerLine.positionCount - 1, pos);
+                currentRootStart = collision;
+                playerLine.positionCount = 1;
+                playerLine.SetPosition(0, collision.point);
+                currentLength = 0;
+                isDrawing = true;
             }
         }
 
-        if(Input.GetMouseButtonUp(0))
+        if (isDrawing)
         {
-            if(rootDrawAnimation != null)
+            if (Input.GetMouseButton(0))
             {
-                StopCoroutine(rootDrawAnimation);
+                float delta = playerLine.positionCount > 1 ? ((Vector2)playerLine.GetPosition(playerLine.positionCount - 1) - pos).magnitude : 0;
+
+                if (currentLength + delta <= maxLength)
+                {
+                    currentLength += delta;
+                    playerLine.positionCount++;
+                    playerLine.SetPosition(playerLine.positionCount - 1, pos);
+                }
             }
-            rootDrawAnimation = StartCoroutine(DrawRoot());
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                Vector3[] positions = new Vector3[playerLine.positionCount];
+                playerLine.GetPositions(positions);
+
+                RootLine root = Instantiate(rootLinePrefab, transform);
+                root.Init(currentRootStart.parent, positions, maxLength, resamplingSize, resamplingNoise);
+                roots.Add(root);
+
+                isDrawing = false;
+            }
         }
     }
 
-    Vector3[] GetSmoothPoints(Vector3[] points)
+    RootCollision GetCollision(Vector2 pos)
     {
-        if(points == null || points.Length<2)
+        RootCollision collision;
+        for (int i=0; i<roots.Count; i++)
         {
-            return new Vector3[0];
-        }
-
-        // Resample points
-        List<Vector3> resampled = new List<Vector3>();
-        float step = resamplingSize * maxLength;
-        float remainingDistance = 0;
-        float delta = 0;
-        float totalDelta = 0;
-        resampled.Add(points[0]);
-        for(int i=1; i<points.Length; i++)
-        {
-            totalDelta = (points[i - 1] - points[i]).magnitude;
-            delta = totalDelta;
-
-            while (delta > step - remainingDistance)
+            collision = roots[i].GetCollision(pos, rootRadius);
+            if(collision != null)
             {
-                delta -= step;
-                remainingDistance = 0;
-                resampled.Add(Vector3.Lerp(points[i - 1], points[i], 1 - (delta / totalDelta)));
+                return collision;
             }
-
-            remainingDistance += delta;
         }
-        resampled.Add(points[points.Length - 1]);
 
-        // Add a touch of noise
-        float noise = resamplingNoise * maxLength;
-        for (int i = 1; i < resampled.Count-1; i++)
+        Vector2 minNode = Vector2.zero;
+        float minDist = Mathf.Infinity;
+        float dist;
+        for (int i = 0; i < startNodes.Length; i++)
         {
-            resampled[i] += Random.insideUnitSphere * noise;
+            dist = Vector2.Distance(pos, startNodes[i].position);
+            if (dist < minDist)
+            {
+                minDist = dist;
+                minNode = startNodes[i].position;
+            }
         }
-
-        // Smooth samples
-        List<Vector3> smoothed = new List<Vector3>();
-        smoothed.Add(points[0]);
-        Vector3[] sampler = new Vector3[5];
-        for(int i=0; i<resampled.Count; i++)
+        if(minDist < startRadius)
         {
-            sampler[0] = i > 1 ? resampled[i - 2] : (i > 0 ? resampled[i - 1] : resampled[i]);
-            sampler[1] = i > 0 ? resampled[i - 1] : resampled[i];
-            sampler[2] = resampled[i];
-            sampler[3] = i < resampled.Count - 1 ? resampled[i + 1] : resampled[i];
-            sampler[4] = i < resampled.Count - 2 ? resampled[i + 2] : (i < resampled.Count - 1 ? resampled[i + 1] : resampled[i]);
+            collision = new RootCollision
+            {
+                parent = null,
+                radius = minDist,
+                point = minNode,
+                maxRadius = startRadius
+            };
 
-            smoothed.Add(
-                (sampler[2] * 0.383f) +
-                ((sampler[1] + sampler[3]) * 0.2417f) +
-                ((sampler[0] + sampler[4]) * 0.0668f)
-                );
+            return collision;
         }
-        smoothed.Add(resampled[resampled.Count - 1]);
 
-        return smoothed.ToArray();
+        return null;
     }
-
-    Coroutine rootDrawAnimation;
-    IEnumerator DrawRoot()
-    {
-        Vector3[] positions = new Vector3[playerLine.positionCount];
-        playerLine.GetPositions(positions);
-        positions = GetSmoothPoints(positions);
-        rootLine.positionCount = positions.Length;
-        rootLine.SetPositions(positions);
-
-        float percent = 0;
-
-        while (percent < 1)
-        {
-            AnimationCurve curve = new AnimationCurve(new Keyframe(0, 1), new Keyframe(percent*0.75f, 1), new Keyframe(percent, 0, -10, .5f));
-            rootLine.widthCurve = curve;
-            percent += Time.deltaTime;
-            yield return null;
-        }
-    }
+    
 }
